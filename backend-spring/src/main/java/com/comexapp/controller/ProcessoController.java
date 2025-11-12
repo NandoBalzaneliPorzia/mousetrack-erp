0fe6684883a9f3932ef3e8f9aa61b8a3933087b2
BeatrizMaranho <anabmaranho@gmail.com>
2025-11-11 21:12:06 -0300

    modificação controller processo

--- backend-spring/src/main/java/com/comexapp/controller/ProcessoController.java
+++ backend-spring/src/main/java/com/comexapp/controller/ProcessoController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/processos")
+@CrossOrigin(origins = "https://mousetrack-frontend.onrender.com")
 public class ProcessoController {
 
     private final ProcessoService service;
