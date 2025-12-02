bbd93a357e9e1d87ddd5504cf84cce33579ffee4
BeatrizMaranho <anabmaranho@gmail.com>
2025-12-01 21:35:35 -0300

    ajustando optional no repository

--- backend-spring/src/main/java/com/comexapp/repository/ProcessoArquivoRepository.java
+++ backend-spring/src/main/java/com/comexapp/repository/ProcessoArquivoRepository.java
@@ -1,7 +1,9 @@
 package com.comexapp.repository;
 
-import com.comexapp.model.ProcessoArquivo;
+import com.comexapp.model.Processo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface ProcessoArquivoRepository extends JpaRepository<ProcessoArquivo, Long> {
+public interface ProcessoRepository extends JpaRepository<Processo, Long> {
+    Optional<Processo> findByCodigo(String codigo);
 }
