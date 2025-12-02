c9a3fe61d24034d38de45ea7289ce33836e88573
BeatrizMaranho <anabmaranho@gmail.com>
2025-12-01 21:44:30 -0300

    repository

--- backend-spring/src/main/java/com/comexapp/repository/ProcessoArquivoRepository.java
+++ backend-spring/src/main/java/com/comexapp/repository/ProcessoArquivoRepository.java
@@ -1,9 +1,7 @@
 package com.comexapp.repository;
 
-import com.comexapp.model.Processo;
+import com.comexapp.model.ProcessoArquivo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface ProcessoRepository extends JpaRepository<Processo, Long> {
-    Optional<Processo> findByCodigo(String codigo);
+public interface ProcessoArquivoRepository extends JpaRepository<ProcessoArquivo, Long> {
 }
--- backend-spring/src/main/java/com/comexapp/repository/ProcessoRepository.java
+++ backend-spring/src/main/java/com/comexapp/repository/ProcessoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProcessoRepository extends JpaRepository<Processo, Long> {
-    Processo findByCodigo(String codigo);
+    Optional<Processo> findByCodigo(String codigo);
 }
+package com.comexapp.service;
